2404d0309662c4e0b40873f4493437e4101bf2dd
deparhtwe <depar3799600@gmail.com>
2026-01-06 05:46:19 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple smooth scroll effect
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+});
